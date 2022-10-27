13c31c7da558bf82f371a94284d68eb59626a3df
Jakub Gut <w65539@student.wsiz.edu.pl>
2022-10-27 09:26:23 +0200

    LAB03

---
+++ Services/Lab03/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿namespace WebAPI.Services.Lab03
+{
+    public class Rectangle: Shape
+    {
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public static Rectangle operator + (Rectangle a, Rectangle b)
+        {
+            return new Rectangle(a.Width + b.Width, a.Height + b.Height);
+        } 
+    }
+}
---
+++ Services/Lab03/Shape.cs
@@ -0,0 +1,29 @@
+﻿namespace WebAPI.Services.Lab03
+{
+    public class Shape
+    {
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public int GetArea()
+            {
+                return Width * Height;
+            }
+
+            public int GetArea(int x)
+            {
+                return Width * Height * x;
+            }
+
+            public int GetArea(double x)
+            {
+                return Width * Height;
+            }
+
+        public int GetShorterSide()
+            {
+                return new List<int>() { Width, Height }.Min();
+            }
+
+    }
+}
