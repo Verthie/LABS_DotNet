f0895bd738a842538f95a2d36bfe0fc95c27f395
Jakub Gut <w65539@student.wsiz.edu.pl>
2022-11-10 11:16:36 +0100

    Lab05

---
+++ Controllers/Lab05/SomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Services.Lab05;
+
+namespace WebAPI.Controllers.Lab05
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SomeController : ControllerBase
+    {
+        [HttpGet]
+        public string CallMethod()
+        {
+            var someInstance = new SomeClass();
+            return someInstance.Print(null);
+        }
+    }
+}
